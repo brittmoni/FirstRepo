e433739e764b699618be62dc1b89a233a4d614d4
Brittany F <brittfranklin92@gmail.com>
2019-08-15 23:32:31 -0400

    Components for app pages

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import './Navbar.css';
+import About from '../../pages/About/About';
+import Help from '../../pages/Help/Help';
+import Volunteer from '../../pages/Volunteer/Volunteer';
+import Contact from '../../pages/Contact/Contact';
 
 function Navbar() {
   return (
     <div>
       <ul>
-        <li><a href='#'>About</a></li>
-        <li><a href='#'>How to Help</a></li>
-        <li><a href='#'>Volunteer Opportunities</a></li>
-        <li><a href='#'>Contact</a></li>
+        <li><a href={About}>About</a></li>
+        <li><a href={Help}>How to Help</a></li>
+        <li><a href={Volunteer}>Volunteer Opportunities</a></li>
+        <li><a href={Contact}>Contact</a></li>
       </ul>
     </div>
   )
