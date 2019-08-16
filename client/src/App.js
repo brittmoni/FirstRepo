8aa390ac5fd34342f2367551c17b6860fe5f9986
Brittany F <brittfranklin92@gmail.com>
2019-08-15 23:49:58 -0400

    Adding react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
-import Home from './pages/Home/Home'
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import Help from './pages/Help/Help';
+import Volunteer from './pages/Volunteer/Volunteer';
+import Contact from './pages/Contact/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Router>
+      <div>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/about' component={About} />
+        <Route exact path='/help' component={Help} />
+        <Route exact path='/volunteer' component={Volunteer} />
+        <Route exact path='/contact' component={Contact} />
+      </div>
+    </Router>
   );
 }
 
