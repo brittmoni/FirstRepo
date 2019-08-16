12fc0270ac9f2a654acde9738fa4fa6dae307ebd
Brittany F <brittfranklin92@gmail.com>
2019-08-15 23:15:19 -0400

    Beginning of the app

---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+function Navbar() {
+  return (
+    <div>
+      <ul>
+        <li>About</li>
+        <li>How to Help</li>
+        <li>Volunteer Opportunities</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Navbar;
---
+++ client/src/pages/Home/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Navbar from '../../components/Navbar/Navbar';
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <h1>Join the Fight against Human Trafficking</h1>
+      </div>
+    )
+  }
+}
+
+export default Home;
