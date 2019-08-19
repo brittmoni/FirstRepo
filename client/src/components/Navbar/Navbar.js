57de4e184a7a04acc46aacc773a43a5bb5afc4ba
brittmoni <brittfranklin92@gmail.com>
2019-08-18 21:44:56 -0400

    Styling to navbar

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,14 +1,11 @@
 import React, { Component } from 'react';
 import './Navbar.css';
-import About from '../../pages/About/About';
-import Help from '../../pages/Help/Help';
-import Volunteer from '../../pages/Volunteer/Volunteer';
-import Contact from '../../pages/Contact/Contact';
 
 function Navbar() {
   return (
     <div>
       <ul>
+        <li><a href='/'>Home</a></li>
         <li><a href='/about'>About</a></li>
         <li><a href='help'>How to Help</a></li>
         <li><a href='/volunteer'>Volunteer Opportunities</a></li>
