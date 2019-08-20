c010de1369fa273b72e07f671abf14b4978c80dc
brittmoni <brittfranklin92@gmail.com>
2019-08-19 21:48:40 -0400

    Addition of the footer

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+  return (
+    <div>
+      <ul>
+        <li><a href='/volunteer'>Volunteer</a></li>
+        <li><a href='/contact'>Contact</a></li>
+      </ul>
+    </div>
+  )
+}
+
+export default Footer;
--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './Home.css';
 import Navbar from '../../components/Navbar/Navbar';
+import Footer from '../../components/Footer/Footer';
 
 class Home extends Component {
   render() {
@@ -9,6 +10,7 @@ class Home extends Component {
         <Navbar />
         <h1>Join the Fight against Human Trafficking</h1>
         <img src={require('../../Images/Stop-Human-Trafficking.jpg')} />
+        <Footer />
       </div>
     )
   }
