34d38cff1eca8933eb7471c7b8689e32b98e54e0
brittmoni <brittfranklin92@gmail.com>
2019-09-12 22:49:46 -0400

    Addition to the form

--- client/src/pages/Contact/Contact.js
+++ client/src/pages/Contact/Contact.js
@@ -8,6 +8,11 @@ function Contact () {
     <div>
       <Navbar />
       <Form>
+        <Form.Group controlId="formName">
+          <Form.Label>Name</Form.Label>
+          <Form.Control type="text" placeholder="Enter name" />
+        </Form.Group>
+
         <Form.Group controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
           <Form.Control type="email" placeholder="Enter email" />
@@ -15,6 +20,15 @@ function Contact () {
             We'll never share your email with anyone else.
           </Form.Text>
         </Form.Group>
+
+        <Form.Group controlId="exampleForm.ControlTextarea1">
+          <Form.Label>Send a message</Form.Label>
+          <Form.Control as="textarea" rows="3" />
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
       </Form>
     </div>
   )
